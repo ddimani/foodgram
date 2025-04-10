26870046aaf92082a8639e42b6febcb54b72cd31
Дмитрий Губский <ddimani@mail.ru>
2025-04-10 08:05:10 +0300

    done

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -10,7 +10,6 @@ from core.constants import RECIPE_FILTER_CHOICES
 from recipes.models import Recipe
 
 
-
 class RecipeFilter(FilterSet):
     is_favorited = TypedChoiceFilter(
         choices=RECIPE_FILTER_CHOICES,
@@ -28,12 +27,12 @@ class RecipeFilter(FilterSet):
         model = Recipe
         fields = ('author',)
 
-    def filter_is_favorited(self, queryset, value):
+    def filter_is_favorited(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
             return queryset.filter(favorites__user=self.request.user)
         return queryset
 
-    def filter_is_in_shopping_cart(self, queryset, value):
+    def filter_is_in_shopping_cart(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
             return queryset.filter(shopping_cart__user=self.request.user)
         return queryset
