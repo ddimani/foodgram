8283297ca7d5cd2ce5992d01164cbc1f5ce8d834
Дмитрий Губский <ddimani@mail.ru>
2025-04-11 04:37:49 +0300

    Done

--- backend/run_app.sh
+++ backend/run_app.sh
@@ -1,9 +1,4 @@
-while ! nc -z db 5432;
-    do sleep .5;
-    echo "wait database";
-done;
-    echo "connected to the database";
-
+python manage.py makemigrations;
 python manage.py migrate;
 python manage.py collectstatic --noinput;
-gunicorn -w 2 -b 0:8000 foodgram.wsgi;
+gunicorn -b 0.0.0.0:8008 foodgram.wsgi;
