1d7d15f05c02ab7418bcd245e33f00446abac78d
Дмитрий Губский <ddimani@mail.ru>
2025-04-12 17:25:14 +0300

    Done

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     re_path(
-        fr'{PREFIX_SHORT_LINK_RECIPE}(?P<short_link>[a-zA-Z]+)/',
+        fr'{PREFIX_SHORT_LINK_RECIPE}(?P<short_link>[a-zA-Z0-9-]+)/',
         redirect_to_recipe
     ),
 ]
