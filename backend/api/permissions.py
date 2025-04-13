44ec3c0e5c9b747e540ee6f3127005d3950c0c1c
Дмитрий Губский <ddimani@mail.ru>
2025-04-13 17:35:45 +0300

    После Ревью

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -14,3 +14,4 @@ class IsAdminAuthorOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.author == request.user or request.user.is_superuser
+    
