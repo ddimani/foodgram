af31fdaf07b6439556762e0c2697e9bfe8cf52d5
Дмитрий Губский <ddimani@mail.ru>
2025-04-13 23:49:39 +0300

    После Ревью.1

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -6,7 +6,7 @@ class IsAdminAuthorOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
 
     def has_object_permission(self, request, view, obj):
         return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_superuser
-                or obj.author == request.user
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_superuser
+            or obj.author == request.user
         )
