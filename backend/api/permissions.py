f5af684867a87b8e2d8bb748f28e358d4e77ab61
Дмитрий Губский <ddimani@mail.ru>
2025-04-13 17:32:27 +0300

    После Ревью

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -4,9 +4,13 @@ from rest_framework import permissions
 class IsAdminAuthorOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
     """Пермишн проверки доступа."""
 
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            return request.user and request.user.is_authenticated
+
+        return super().has_permission(request, view)
+
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_superuser
-            or obj.author == request.user
-        )
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user or request.user.is_superuser
